9b25e68e18228dfa108126050b7355c9531d9ade
AllesBlaw <103613091+NickSigler@users.noreply.github.com>
2023-05-30 12:00:33 -0300

    sd

--- src/Model/Gerente.java
+++ src/Model/Gerente.java
@@ -7,7 +7,7 @@ public class Gerente extends Funcionario implements Serializable {
     public String email;
 
 
-    public Gerente(double cpf, String nome, int registro, String login, char[] senha, String senha1) {
+    public Gerente(String cpf, String nome, char[] registro, String login, char[] senha) {
         super(cpf, nome, registro, login, senha);
 
     }
