01f96bd17e99f3d70906776ad622a1c0ff349a94
eduardoeleuterio <eduardor.eleuterio@gmail.com>
2023-05-30 09:52:15 -0300

    categoria

--- src/Controller/CriaCategoria.java
+++ src/Controller/CriaCategoria.java
@@ -1,5 +1,18 @@
 package Controller;
+import java.io.IOException;
+import Model.Categoria;
 
 public class CriaCategoria {
+    public static void main(String[] args){
+        Categoria categoria = new Categoria( "Pizza Especial");
+        
+        try {
+            Serializador.gravar("Categoria.ser", categoria);
+            System.out.println("Categoria gravado");
+        } catch (IOException e) {
+            System.out.println("IOException");
+            e.printStackTrace();
+        }
+    }
     
 }
--- src/Model/Categoria.java
+++ src/Model/Categoria.java
@@ -1,5 +1,13 @@
 package Model;
+import java.io.Serializable;
 
-public class Categoria {
+public class Categoria implements Serializable{
     public String nome;
+
+    public Categoria(String nome){
+        this.nome = nome;
+    }
+    public void exibir(){
+        System.out.println("Categoria" + nome);
+    }
 }
