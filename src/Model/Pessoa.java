e87023bde96b56f5f4786f985780d456fc679283
Gabriel Fernandes <cafegabriel1@gmail.com>
2023-05-30 09:31:03 -0300

    cliente e funcionario

--- src/Model/Pessoa.java
+++ src/Model/Pessoa.java
@@ -2,32 +2,11 @@ package Model;
 
 public abstract class Pessoa  {
 
-    public String nome;
-    public Integer senha;
-    public String email;
     public Double cpf;
+    public String nome;
 
-    public Pessoa(String nome, int senha, String email, double cpf) {
+    public Pessoa(double cpf, String nome) {
         this.nome = nome;
-        this.senha = senha;
-        this.email = email;
         this.cpf = cpf;
-    }
-
-
-
-    public Double getCpf() {
-        return cpf;
-    }
-
-
-
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
     }
 }
