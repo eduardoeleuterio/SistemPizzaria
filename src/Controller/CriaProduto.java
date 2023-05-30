f9328b06d146eee1d0356b1aaacc110b17851666
eduardoeleuterio <eduardor.eleuterio@gmail.com>
2023-05-30 10:40:53 -0300

    criar produto

--- src/Controller/CriaProduto.java
+++ src/Controller/CriaProduto.java
@@ -1,5 +1,28 @@
 package Controller;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import Model.Categoria;
+import Model.Ingrediente;
+import Model.Produto;
 
 public class CriaProduto {
+    public static void main(String[] args){
+        Categoria categoria = new Categoria( "Pizza Tradicional");
+        Ingrediente ingrediente = new Ingrediente( "Queijo Mussarela");
+        List<Ingrediente> ingredientes = new ArrayList<Ingrediente>();
+        ingredientes.add(0, ingrediente);
+
+        Produto produto = new Produto(1, "PizzaCalabresa",categoria, 10.00 , ingredientes);
+        //(int codigo, String nome, Categoria categoria, double preco, List<Ingrediente> ingredientes
+        try {
+            Serializador.gravar("Produto.ser", produto);
+            System.out.println("Produto gravado"+ produto);
+        } catch (IOException e) {
+            System.out.println("IOException");
+            e.printStackTrace();
+        }
+    }
     
 }
--- src/Controller/RecuperarProduto.java
+++ src/Controller/RecuperarProduto.java
@@ -1,5 +1,20 @@
 package Controller;
+import java.io.IOException;
+
+import Model.Produto;
 
 public class RecuperarProduto {
-    
+    public static void main(String[] args) {
+        try {
+            Produto produto = (Produto) Serializador.ler("Produto.ser");
+            System.out.println("Produto recuperada  -");
+            produto.exibir();
+        } catch (IOException e) {
+            System.out.println("IOException");
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            System.out.println("ClassNotFoundException");
+            e.printStackTrace();
+        }
+    }
 }
