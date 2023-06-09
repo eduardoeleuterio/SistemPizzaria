6ec59709ae2ce8204d3e2d327a3e395e0e963441
AllesBlaw <103613091+NickSigler@users.noreply.github.com>
2023-06-09 11:05:05 -0300

    Update Serializador.java

--- src/Controller/Serializador.java
+++ src/Controller/Serializador.java
@@ -1,22 +1,36 @@
 package Controller;
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Serializador {
-    public static void gravar(String nome_arquivo, Object objeto) throws IOException {
+
+    public static void gravar(String nome_arquivo, Object objeto) throws IOException, ClassNotFoundException {
+        List<Object> objetosAntigos = new ArrayList<>();
+
+        File arquivoExistente = new File(nome_arquivo);
+        if (arquivoExistente.exists()) {
+            objetosAntigos = ler(nome_arquivo);
+        }
+
+        objetosAntigos.add(objeto);
+
         FileOutputStream arquivo = new FileOutputStream(nome_arquivo);
         ObjectOutputStream gravador = new ObjectOutputStream(arquivo);
-        gravador.writeObject(objeto);
+        gravador.writeObject(objetosAntigos);
         gravador.close();
         arquivo.close();
     }
 
-    public static Object ler(String nome_arquivo) throws IOException, ClassNotFoundException {
+    @SuppressWarnings("unchecked")
+    public static List<Object> ler(String nome_arquivo) throws IOException, ClassNotFoundException {
         FileInputStream arquivo = new FileInputStream(nome_arquivo);
         ObjectInputStream restaurador = new ObjectInputStream(arquivo);
-        Object objeto = restaurador.readObject();
+        List<Object> objetos = (List<Object>) restaurador.readObject();
         restaurador.close();
         arquivo.close();
-        return objeto;
-    }   
+        return objetos;
+    }
+
 }
