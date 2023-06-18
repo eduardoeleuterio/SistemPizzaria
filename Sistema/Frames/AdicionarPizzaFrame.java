db4b63b222fcd12613951106bb3d91a7b759a144
AllesBlaw <103613091+NickSigler@users.noreply.github.com>
2023-06-18 14:51:00 -0300

    att

--- Sistema/Frames/AdicionarPizzaFrame.java
+++ Sistema/Frames/AdicionarPizzaFrame.java
@@ -1,5 +1,3 @@
-package Sistema.Frames;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -10,80 +8,104 @@ import Sistema.Controller.*;
 import Sistema.Model.*;
 
 public class AdicionarPizzaFrame extends JFrame {
-    private List<Ingrediente> ingredientes;
-    private JTextField txtNomePizza;
-    private JTextField txtCodigoProduto;
-    private JTextField txtNomeProduto;
-    private JTextField txtPrecoProduto;
-    private DefaultListModel<Ingrediente> ingredientListModel;
-    private JList<Ingrediente> ingredientList;
+    private JTextField codigoTextField;
+    private JTextField nomeTextField;
+    private JTextField precoTextField;
+    private JButton adicionarButton;
+    private DefaultListModel<Ingrediente> ingredientesListModel;
+    private JList<Ingrediente> ingredientesList;
+    private JButton adicionarIngredienteButton;
 
-    public AdicionarPizzaFrame(List<Ingrediente> ingredientes) {
-        this.ingredientes = ingredientes;
+    private ProdutoController produtoController;
+
+    public AdicionarPizzaFrame() {
+        produtoController = new ProdutoController();
 
         setTitle("Adicionar Pizza");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(400, 300);
-        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        setLayout(new GridLayout(3, 1));
+        setLayout(new BorderLayout());
 
-        JPanel panelPizza = new JPanel(new GridLayout(2, 2));
-        JLabel lblNomePizza = new JLabel("Nome da Pizza:");
-        txtNomePizza = new JTextField();
-        panelPizza.add(lblNomePizza);
-        panelPizza.add(txtNomePizza);
+        JPanel formPanel = new JPanel(new GridLayout(0, 2));
 
-        JPanel panelIngredientes = new JPanel(new BorderLayout());
-        JLabel lblIngredientes = new JLabel("Ingredientes:");
-        ingredientListModel = new DefaultListModel<>();
-        ingredientList = new JList<>(ingredientListModel);
-        JScrollPane ingredientScrollPane = new JScrollPane(ingredientList);
-        panelIngredientes.add(lblIngredientes, BorderLayout.NORTH);
-        panelIngredientes.add(ingredientScrollPane, BorderLayout.CENTER);
+        JLabel codigoLabel = new JLabel("Código:");
+        codigoTextField = new JTextField();
+        formPanel.add(codigoLabel);
+        formPanel.add(codigoTextField);
 
-        JPanel panelProduto = new JPanel(new GridLayout(3, 2));
-        JLabel lblCodigoProduto = new JLabel("Código do Produto:");
-        txtCodigoProduto = new JTextField();
-        JLabel lblNomeProduto = new JLabel("Nome do Produto:");
-        txtNomeProduto = new JTextField();
-        JLabel lblPrecoProduto = new JLabel("Preço do Produto:");
-        txtPrecoProduto = new JTextField();
-        panelProduto.add(lblCodigoProduto);
-        panelProduto.add(txtCodigoProduto);
-        panelProduto.add(lblNomeProduto);
-        panelProduto.add(txtNomeProduto);
-        panelProduto.add(lblPrecoProduto);
-        panelProduto.add(txtPrecoProduto);
+        JLabel nomeLabel = new JLabel("Nome:");
+        nomeTextField = new JTextField();
+        formPanel.add(nomeLabel);
+        formPanel.add(nomeTextField);
 
-        JButton btnAdicionar = new JButton("Adicionar");
-        btnAdicionar.addActionListener(new ActionListener() {
+        JLabel precoLabel = new JLabel("Preço:");
+        precoTextField = new JTextField();
+        formPanel.add(precoLabel);
+        formPanel.add(precoTextField);
+
+        JLabel ingredientesLabel = new JLabel("Ingredientes:");
+        ingredientesListModel = new DefaultListModel<>();
+        ingredientesList = new JList<>(ingredientesListModel);
+        JScrollPane ingredientesScrollPane = new JScrollPane(ingredientesList);
+        formPanel.add(ingredientesLabel);
+        formPanel.add(ingredientesScrollPane);
+
+        adicionarIngredienteButton = new JButton("Adicionar Ingrediente");
+        adicionarIngredienteButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String nomePizza = txtNomePizza.getText();
-                int codigoProduto = Integer.parseInt(txtCodigoProduto.getText());
-                String nomeProduto = txtNomeProduto.getText();
-                double precoProduto = Double.parseDouble(txtPrecoProduto.getText());
-
-                List<Ingrediente> ingredientesSelecionados = ingredientList.getSelectedValuesList();
-
-                Produto produto = new Produto(codigoProduto, nomeProduto, precoProduto, ingredientesSelecionados);
-                // Adicione o produto à lista de produtos
-
-                JOptionPane.showMessageDialog(null, "Pizza e Produto adicionados com sucesso!");
-
-                // Limpe os campos de entrada
-                txtNomePizza.setText("");
-                txtCodigoProduto.setText("");
-                txtNomeProduto.setText("");
-                txtPrecoProduto.setText("");
-                ingredientList.clearSelection();
+                adicionarIngrediente();
             }
         });
+        formPanel.add(adicionarIngredienteButton);
 
-        add(panelPizza);
-        add(panelIngredientes);
-        add(panelProduto);
-        add(btnAdicionar);
+        add(formPanel, BorderLayout.CENTER);
+
+        adicionarButton = new JButton("Adicionar");
+        adicionarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                adicionarPizza();
+            }
+        });
+        add(adicionarButton, BorderLayout.SOUTH);
     }
-    
-}
 
+    private void adicionarIngrediente() {
+        String nomeIngrediente = JOptionPane.showInputDialog(this, "Digite o nome do ingrediente:");
+        if (nomeIngrediente != null && !nomeIngrediente.isEmpty()) {
+            Ingrediente ingrediente = new Ingrediente(nomeIngrediente);
+            ingredientesListModel.addElement(ingrediente);
+        }
+    }
+
+    private void adicionarPizza() {
+        int codigo = Integer.parseInt(codigoTextField.getText());
+        String nome = nomeTextField.getText();
+        double preco = Double.parseDouble(precoTextField.getText());
+        List<Ingrediente> ingredientes = new ArrayList<>();
+        for (int i = 0; i < ingredientesListModel.size(); i++) {
+            ingredientes.add(ingredientesListModel.getElementAt(i));
+        }
+
+        produtoController.criarProduto(codigo, nome, preco, ingredientes);
+
+        JOptionPane.showMessageDialog(this, "Pizza adicionada com sucesso!");
+
+        // Limpar campos e lista de ingredientes
+        codigoTextField.setText("");
+        nomeTextField.setText("");
+        precoTextField.setText("");
+        ingredientesListModel.clear();
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                AdicionarPizzaFrame pizzaFrame = new AdicionarPizzaFrame();
+                pizzaFrame.setVisible(true);
+            }
+        });
+    }
+}
