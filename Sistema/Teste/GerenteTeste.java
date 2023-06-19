8fa8d509fff6a64272d99d7064ad89248e123f5a
Angelo <angelsseq@gmail.com>
2023-06-19 08:55:43 -0300

    Update GerenteTeste.java

--- Sistema/Teste/GerenteTeste.java
+++ Sistema/Teste/GerenteTeste.java
@@ -8,29 +8,29 @@ public class GerenteTeste {
     public static void main(String[] args) {
         GerenteController gerenteController = new GerenteController();
 
-        // Teste de criação de gerente
+        //  Teste de criação de gerente
         gerenteController.criarGerente("12345678901", "João", "joao123", "senha123", "Vendas");
         gerenteController.criarGerente("98765432109", "Maria", "maria456", "123456", "RH");
 
-        // Teste de leitura de gerente
+        //  Teste de leitura de gerente
         System.out.println("===== Ler Gerente =====");
         Gerente gerente1 = gerenteController.lerGerente("12345678901");
         Gerente gerente2 = gerenteController.lerGerente("98765432109");
         exibirGerente(gerente1);
         exibirGerente(gerente2);
 
-        // Teste de atualização de gerente
+        //  Teste de atualização de gerente
         gerenteController.atualizarGerente("12345678901", "João da Silva", "joao.silva", "novasenha", "Financeiro");
 
-        // Teste de leitura após atualização
+        //  Teste de leitura após atualização
         System.out.println("===== Ler Gerente após atualização =====");
         gerente1 = gerenteController.lerGerente("12345678901");
         exibirGerente(gerente1);
 
-        // Teste de exclusão de gerente
+        //  Teste de exclusão de gerente
         gerenteController.deletarGerente("98765432109");
 
-        // Teste de leitura após exclusão
+        //  Teste de leitura após exclusão
         System.out.println("===== Ler Gerente após exclusão =====");
         gerente2 = gerenteController.lerGerente("98765432109");
         exibirGerente(gerente2);
