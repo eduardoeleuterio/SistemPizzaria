99a5a9c5fd3b372a29d7395517c2818314682f49
Angelo <angelsseq@gmail.com>
2023-06-19 08:52:12 -0300

    Update EntregadorTeste.java

--- Sistema/Teste/EntregadorTeste.java
+++ Sistema/Teste/EntregadorTeste.java
@@ -7,18 +7,18 @@ public class EntregadorTeste {
     public static void main(String[] args) {
         EntregadorController entregadorController = new EntregadorController();
 
-        // Teste de criação de entregador
+        // Teste de criação de entregador.
         entregadorController.criarEntregador("12345678901", "João", "joao123", "senha123", "Moto");
         entregadorController.criarEntregador("98765432109", "Maria", "maria456", "senha456", "Bicicleta");
 
-        // Teste de leitura de entregador
+        // Teste de leitura de entregador.
         System.out.println("===== Ler Entregador =====");
         Entregador entregador1 = entregadorController.lerEntregador("12345678901");
         Entregador entregador2 = entregadorController.lerEntregador("98765432109");
         exibirEntregador(entregador1);
         exibirEntregador(entregador2);
 
-        // Teste de atualização de entregador
+        // Teste de atualização de entregador.
         entregadorController.atualizarEntregador("12345678901", "João da Silva", "joaosilva", "novasenha", "Carro");
 
         // Teste de leitura após atualização
@@ -26,7 +26,7 @@ public class EntregadorTeste {
         entregador1 = entregadorController.lerEntregador("12345678901");
         exibirEntregador(entregador1);
 
-        // Teste de exclusão de entregador
+        // Teste de exclusão de entregador.
         entregadorController.deletarEntregador("98765432109");
 
         // Teste de leitura após exclusão
