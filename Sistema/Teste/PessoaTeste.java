1445f0c4760c5472b43c3e02703adebc2a556e72
Angelo <angelsseq@gmail.com>
2023-06-19 08:56:59 -0300

    Update PessoaTeste.java

--- Sistema/Teste/PessoaTeste.java
+++ Sistema/Teste/PessoaTeste.java
@@ -8,18 +8,18 @@ public class PessoaTeste {
      public static void main(String[] args) {
         PessoaController pessoaController = new PessoaController();
 
-        // Teste de criação de pessoa
+        // Teste de criação de pessoa 
         pessoaController.criarPessoa("12345678901", "João");
         pessoaController.criarPessoa("98765432109", "Maria");
 
-        // Teste de leitura de pessoa
+        // Teste de leitura de pessoa  
         System.out.println("===== Ler Pessoa =====");
         Pessoa pessoa1 = pessoaController.lerPessoa("12345678901");
         Pessoa pessoa2 = pessoaController.lerPessoa("98765432109");
         exibirPessoa(pessoa1);
         exibirPessoa(pessoa2);
 
-        // Teste de atualização de pessoa
+        // Teste de atualização de pessoa 
         pessoaController.atualizarPessoa("12345678901", "João da Silva");
 
         // Teste de leitura após atualização
@@ -27,7 +27,7 @@ public class PessoaTeste {
         pessoa1 = pessoaController.lerPessoa("12345678901");
         exibirPessoa(pessoa1);
 
-        // Teste de exclusão de pessoa
+        // Teste de exclusão de pessoa 
         pessoaController.deletarPessoa("98765432109");
 
         // Teste de leitura após exclusão
